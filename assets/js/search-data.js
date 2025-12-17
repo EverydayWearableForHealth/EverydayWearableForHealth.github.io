510e10e1e0fb17a89db5397becc2fdfc86747a99
chankyu-charlie <chankyu@uw.edu>
2025-12-17 07:40:30 +0000

    Deploying to gh-pages from @ EverydayWearableForHealth/EverydayWearableForHealth.github.io@f6a46beef98e7d4552b01ae78c6ee3e0a8f0cff5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/organizers/";
           },
-        },{id: "nav-cfp",
-          title: "CFP",
-          description: "Call for Participation",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cfp/";
-          },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -64,10 +57,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-workshop-cfp-released-looking-forward-to-your-submissions",
-          title: 'Workshop CFP released! Looking forward to your submissions!',
-          description: "",
-          section: "News",},{id: "news-chi-workshop-accepted",
+            },},{id: "news-chi-workshop-accepted",
           title: 'CHI Workshop Accepted!',
           description: "",
           section: "News",},{

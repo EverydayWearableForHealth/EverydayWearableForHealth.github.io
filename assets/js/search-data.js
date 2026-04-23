d865e88857b66a679d84923ebf1159e6121c68e0
chankyu-charlie <chankyu@uw.edu>
2026-04-23 19:26:00 +0000

    Deploying to gh-pages from @ EverydayWearableForHealth/EverydayWearableForHealth.github.io@5d623f92ff172d76ab67ffdd9335597129734709 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-schedule",
           title: "Schedule",
-          description: "Tentative Schedule of the two-session Workshop",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/schedule/";
@@ -36,6 +36,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cfp/";
+          },
+        },{id: "nav-workshop",
+          title: "Workshop",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/workshop/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
